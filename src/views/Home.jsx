5261b23562785d2e9183594d45395f922920891c
Jdcisneros <jorge_08.8@hotmail.com>
2024-08-07 19:04:38 -0300

    porfolio

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,3 +1,4 @@
+import  { useState } from 'react';
 import Footer from "../components/Footer";
 import Nav from "../components/Nav";
 import Socials from "../components/Socials";
@@ -7,6 +8,17 @@ import image2 from "/image2.jpg";
 import image3 from "/image3.jpg";
 
 function Home() {
+  const images = [image1, image2, image3];
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const nextSlide = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+  };
+
+  const prevSlide = () => {
+    setCurrentIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
+  };
+
   return (
     <div className="flex justify-center flex-col items-center bg-stone-100">
       <Nav />
@@ -17,15 +29,11 @@ function Home() {
         className=" lg:w-[740px]  md:w-[440px] flex justify-center items-center z-30 "
         id="top"
       >
-        
-
         <img
           src="/imagenF.png"
           alt="imagenPerfil"
           className="lg:w-3/4 w-full object-cover mt-20 animate-slide-in-left "
         />
-    
-
         <div className=" absolute bottom-36 text-center md:static lg:static text-nowrap flex flex-col">
           <h2 className=" text-xs lg:text-3xl font-semibold text-stone-700 animate-slide-in-right ">
             ESTUDIANTE DE DISEÑO DE INDUMENTARIA
@@ -33,7 +41,7 @@ function Home() {
           <h1 className=" text-[56px] lg:text-9xl font-font text-lime-500 animate-slide-in-right ">
             GUILLERMINA BASCOY
           </h1>
-          <p className="text-xs px-5 text-pretty animate-slide-in-right transform duration-3000 ">
+          <p className="text-xs  w-[340px] px-5 text-pretty animate-slide-in-right transform duration-3000 ">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vel
             metus tellus. Etiam blandit aliquet lorem nec varius. Nulla
             tincidunt enim ac sem ornare bibendum. Mauris iaculis hendrerit
@@ -42,71 +50,34 @@ function Home() {
           </p>
         </div>
       </section>
-      <section
-        id="portfolio"
-        className=" lg:w-full md:w-[440px] flex justify-center flex-col items-center z-30 bg-stone-950 mt-32"
-      >
-        <h2 className="animate-slide-in-left font-bold text-7xl text-lime-500 mt-16">
+      <section id="portfolio" className="lg:w-full md:w-[440px] flex justify-center flex-col items-center z-30 bg-stone-950">
+        <h2 className="animate-slide-in-left font-bold lg:text-7xl text-6xl text-lime-500 mt-16">
           PORTFOLIO
         </h2>
-        <div className="flex justify-center space-x-4 mx-32 my-8">
-          <div className="relative w-1/3 overflow-hidden group rounded">
-            <img
-              src={image1}
-              alt="image1"
-              className="object-cover h-[500px] w-full transition-transform duration-300 ease-in-out transform scale-110 group-hover:scale-100 group-hover:translate-x-0 group-hover:translate-y-0"
-            />
-            <div className="absolute inset-0 bg-gradient-to-b from-lime-50/70 to-stone-950/70 bg-opacity-50 flex flex-col items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300 border-animate ">
-              <div className="absolute bottom-10 flex flex-col justify-center items-center">
-                <p className="text-stone-50 font-bold text-center text-xl text-pretty  ">
-                  NOMBRE PROJECTO
-                </p>
-                <a>
-                  <button className="py-1 px-5 my-10 border-2 rounded border-stone-50 hover:bg-lime-500 text-stone-50 font-bold hover:border-lime-500 transition-colors duration-300 transform hover:translate-y-[-2px]">
-                    VER MÁS
-                  </button>
-                </a>
+        <div className="relative w-full overflow-hidden">
+          <div className="flex transition-transform duration-300 ease-in-out" style={{ transform: `translateX(-${currentIndex * 100}%)` }}>
+            {images.map((image, index) => (
+              <div key={index} className="carousel-slide relative w-full flex-shrink-0">
+                <img src={image} alt={`image${index + 1}`} className="object-cover h-[500px] w-full" />
+                <div className="absolute inset-0 bg-gradient-to-b from-lime-50/70 to-stone-950/70 bg-opacity-50 flex flex-col items-center justify-center opacity-0 hover:opacity-100 transition-opacity duration-300">
+                  <div className="absolute bottom-10 flex flex-col justify-center items-center">
+                    <p className="text-stone-50 font-bold text-center text-xl text-pretty">NOMBRE PROJECTO</p>
+                    <a>
+                      <button className="py-1 px-5 my-10 border-2 rounded border-stone-50 hover:bg-lime-500 text-stone-50 font-bold hover:border-lime-500 transition-colors duration-300 transform hover:translate-y-[-2px]">
+                        VER MÁS
+                      </button>
+                    </a>
+                  </div>
+                </div>
               </div>
-            </div>
+            ))}
           </div>
-          <div className="relative w-1/3 overflow-hidden group rounded">
-            <img
-              src={image2}
-              alt="image2"
-              className="object-cover h-[500px]  w-full transition-transform duration-300 ease-in-out transform scale-110 group-hover:scale-100 group-hover:translate-x-0 group-hover:translate-y-0"
-            />
-            <div className="absolute inset-0 bg-gradient-to-b from-lime-50/70 to-stone-950/70 bg-opacity-50 flex flex-col items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300 border-animate ">
-              <div className="absolute bottom-10 flex flex-col justify-center items-center">
-                <p className="text-stone-50 font-bold text-center text-xl text-pretty  ">
-                  NOMBRE PROJECTO
-                </p>
-                <a>
-                  <button className="py-1 px-5 my-10 border-2 rounded border-stone-50 hover:bg-lime-500 text-stone-50 font-bold hover:border-lime-500 transition-colors duration-300 transform hover:translate-y-[-2px]">
-                    VER MÁS
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
-          <div className="relative w-1/3 overflow-hidden group rounded">
-            <img
-              src={image3}
-              alt="image3"
-              className="object-cover h-[500px]  w-full transition-transform duration-300 ease-in-out transform scale-110 group-hover:scale-100 group-hover:translate-x-0 group-hover:translate-y-0"
-            />
-            <div className="absolute inset-0 bg-gradient-to-b from-lime-50/70 to-stone-950/70 bg-opacity-50 flex flex-col items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300 border-animate ">
-              <div className="absolute bottom-10 flex flex-col justify-center items-center">
-                <p className="text-stone-50 font-bold text-center text-xl text-pretty  ">
-                  NOMBRE PROJECTO
-                </p>
-                <a>
-                  <button className="py-1 px-5 my-10 border-2 rounded border-stone-50 hover:bg-lime-500 text-stone-50 font-bold hover:border-lime-500 transition-colors duration-300 transform hover:translate-y-[-2px]">
-                    VER MÁS
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
+          <button className="absolute top-1/2 left-0 transform -translate-y-1/2 bg-stone-50 text-lime-500 p-2 rounded-full z-10" onClick={prevSlide}>
+            &lt;
+          </button>
+          <button className="absolute top-1/2 right-0 transform -translate-y-1/2 bg-stone-50 text-lime-500 p-2 rounded-full z-10" onClick={nextSlide}>
+            &gt;
+          </button>
         </div>
         <a>
           <button className="py-1 px-5 my-10 border-2 rounded border-stone-50 hover:bg-lime-500 text-stone-50 font-bold hover:border-lime-500 transition-colors duration-300 transform hover:translate-y-[-2px]">
@@ -114,10 +85,7 @@ function Home() {
           </button>
         </a>
       </section>
-      <section
-        id="blogs"
-       className="w-[340px] lg:w-full md:w-[440px] flex justify-center flex-col items-center z-30"
-      >
+      <section id="blogs" className="w-[340px] lg:w-full md:w-[440px] flex justify-center flex-col items-center z-30">
         <h2 className="animate-slide-in-left font-bold text-7xl text-lime-500 mt-20">BLOGS</h2>
         <div className="flex justify-center space-x-4 mx-32 my-8">
           <div className="relative w-1/3 overflow-hidden group rounded">
@@ -201,8 +169,7 @@ function Home() {
           orci at, commodo nibh.
         </p>
       </section>
-        <Footer/>
-
+      <Footer/>
       <Socials/>
     </div>
   );
