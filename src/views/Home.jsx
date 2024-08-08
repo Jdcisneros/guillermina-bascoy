ad3180185aec1ba1bc3947ed01d73f21badc6bb3
Jdcisneros <jorge_08.8@hotmail.com>
2024-08-08 16:03:46 -0300

    arreglos

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -34,16 +34,16 @@ function Home() {
         <img
           src="/imagenF.png"
           alt="imagenPerfil"
-          className="lg:w-3/4 w-full object-cover mt-32 animate-slide-in-left"
+          className="lg:w-3/4 w-full object-cover mt-24 animate-slide-in-left"
         />
-        <div className="absolute bottom-0 text-center md:static lg:static lg:text-nowrap flex justify-center flex-col items-center" style={{ textShadow: '1px 1px 2px rgba(0, 0, 0, 0.3)' }}>
+        <div className="absolute -bottom-8 text-center md:static lg:static lg:text-nowrap flex justify-center flex-col items-center" style={{ textShadow: '1px 1px 2px rgba(0, 0, 0, 0.3)' }}>
           <h2 className="text-xs lg:text-3xl font-semibold text-stone-950 animate-slide-in-right">
             ESTUDIANTE DE DISEÑO DE INDUMENTARIA
           </h2>
           <h1 className="text-5xl lg:text-9xl font-font text-lime-500 animate-slide-in-right">
             GUILLERMINA BASCOY
           </h1>
-          <p className="text-pretty animate-slide-in-right transform duration-3000 w-[340px] lg:w-[740px] text-center  text-stone-950 mb-10">
+          <p className=" text-xs text-pretty animate-slide-in-right transform duration-3000 w-[340px] lg:w-[740px] text-center  text-stone-950 mb-10">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vel
         metus tellus. Etiam blandit aliquet lorem nec varius. Nulla
         tincidunt enim ac sem ornare bibendum. Mauris iaculis hendrerit
